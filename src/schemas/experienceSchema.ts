a64a19dee2ff9b74c6536a978f4660b72e7517d0
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-08-15 05:25:38 -0300

    fix: fix schema

--- src/schemas/experienceSchema.ts
+++ src/schemas/experienceSchema.ts
@@ -2,9 +2,9 @@ import joi from "joi";
 import { CreateExperienceData } from "../repositories/experienceRepositry.js";
 
 export const experienceSchema = joi.object<CreateExperienceData>({
-  title: joi.string().required().max(20),
+  title: joi.string().required().max(30),
   place: joi.string().required().max(20),
   date: joi.string().required().min(10),
-  description: joi.string().required().max(30),
+  description: joi.string().required().max(40),
   category_id: joi.number().required()
 });
--- src/schemas/plannedExperienceSchema.ts
+++ src/schemas/plannedExperienceSchema.ts
@@ -2,9 +2,9 @@ import joi from "joi";
 import { CreatePlannedExperienceData } from "../repositories/plannedExperienceRepository.js";
 
 export const plannedExperienceSchema = joi.object<CreatePlannedExperienceData>({
-  title: joi.string().required().max(20),
+  title: joi.string().required().max(30),
   place: joi.string().required().max(20),
-  date: joi.string().required().min(8),
-  description: joi.string().required().max(30),
+  date: joi.string().required().min(10),
+  description: joi.string().required().max(40),
   category_id: joi.number().required()
 });
